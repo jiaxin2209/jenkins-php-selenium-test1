da9eeea2647be83fa6b6daf44c9e110e8953d7fe
jiaxin2209 <2102665@sit.singaporetech.edu.sg>
2023-11-09 01:48:14 +0800

    Update deploy.sh

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env sh
 
 set -x
+<<<<<<< HEAD
 docker run -d -p 80:80 --name my-apache-php-app -v https://github.com/jiaxin2209/jenkins-php-selenium-test1.git
+=======
+# docker run -d -p 80:80 --name my-apache-php-app -v c:\\...\\jenkins-php-selenium-test\\src:/var/www/html php:7.2-apache
+docker run -d -p 80:80 --name my-apache-php-app -v C:/Users/Jia Xin/Documents/GitHub/jenkins-php-selenium-test
+# C:\Users\Jia Xin\Documents\GitHub\jenkins-php-selenium-test
+>>>>>>> parent of de3cb40 (Update deploy.sh)
 sleep 1
 set +x
 

a0b916e2051d8a9a8c9f79d9a1f9b981e46cc5ea
jiaxin2209 <2102665@sit.singaporetech.edu.sg>
2023-11-08 23:31:58 +0800

    Make jenkins/scripts/kill.sh executable

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -7,7 +7,6 @@ docker run -d -p 80:80 --name my-apache-php-app -v C:/Users/Jia Xin/Documents/Gi
 sleep 1
 set +x
 
-
 echo 'Now...'
 echo 'Visit http://localhost to see your PHP application in action.'
 

7bb60247af62407df8a0e01d11ec63599da93337
jiaxin2209 <2102665@sit.singaporetech.edu.sg>
2023-11-11 16:54:56 +0800

    removed com

--- src/test/java/mycompany/app/AppTest.java
+++ src/test/java/mycompany/app/AppTest.java
@@ -1,11 +1,11 @@
-package com.mycompany.app;
+package mycompany.app;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.After;
-import static org.junit.Assert.*;
+import static org.junit.Assert.*;	
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;

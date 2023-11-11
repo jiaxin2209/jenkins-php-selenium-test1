50a67f39f03357fc0a7c459781de7057a8ff0d75
jiaxin2209 <2102665@sit.singaporetech.edu.sg>
2023-11-11 16:25:21 +0800

    Add initial files

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-
+# changes
 set -x
 docker run -d -p 80:80 --name my-apache-php-app -v /var/jenkins_home/workspace/jenkins-php-selenium-test1/src:/var/www/html php:7.2-apache
 sleep 1

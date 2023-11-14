b69ad54344ead6d8e024420955baa2cf2fe606dc
jiaxin2209 <91715894+jiaxin2209@users.noreply.github.com>
2023-11-15 02:02:22 +0800

    change to path

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -2,7 +2,7 @@
 # changes
 # 
 set -x
-docker run -d -p 80:80 --name my-apache-php-app -v /var/jenkins_home/workspace/jenkins-php-selenium-test1/src:/var/www/html php:7.2-apache
+docker run -d -p 80:80 --name my-apache-php-app -v /var/jenkins_home/workspace/lab-x07b/src⁠:/var/www/html php:7.2-apache
 sleep 1
 set +x
 
